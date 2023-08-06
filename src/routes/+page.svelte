35b64a69e6df286a46038407308e67a406b8afeb
EnterOrion <orionwesson@gmail.com>
2023-08-06 12:49:34 -0500

    Basic nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,1 @@
 <h1>Hello, Svelte</h1>
-<a href="/about">About</a>
-<a href="/posts">Posts</a>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,1 @@
 <h1>This is my very first Svelte project!</h1>
-<a href="/">Home</a>
-<a href="/posts">Posts</a>
