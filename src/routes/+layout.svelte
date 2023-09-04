22e6839b1ee1bf97aae0be9d8f6e8a9f7023da39
EnterOrion <orionwesson@gmail.com>
2023-09-04 12:23:16 -0500

    Very basic home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,14 +7,73 @@
 <slot />
 
 <style>
+  /* CSS Reset */
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  body,
+  html {
+    height: 100%;
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    overflow-wrap: break-word;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    font-weight: inherit;
+    font-style: inherit;
+    font-size: 100%;
+    font-family: inherit;
+    vertical-align: baseline;
+  }
+
+  #root,
+  #__next {
+    isolation: isolate;
+  }
+
   :global(body) {
     /* background-color: rgb(58, 84, 71); */
     background-color: rgb(25, 25, 25);
     color: rgb(255, 255, 255);
-    height: 100vh;
-    width: 100vw;
     padding: 0;
     margin: 0;
+    height: 100%;
+    width: 100%;
   }
 
   nav {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,35 @@
 </script>
 
 <main>
-  <p>Hello, again.</p>
+  <h1>&gt; Orion Wesson</h1>
+  <h2>Developer, writer, designer.</h2>
 </main>
 
 <style>
+  main {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  h1,
+  h2 {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+  }
+
+  h1 {
+    font-size: 5rem;
+  }
+
+  h2 {
+    font-size: 2.2rem;
+    margin-top: 0.4rem;
+    font-style: italic;
+    padding-left: 1.5rem;
+  }
 </style>
